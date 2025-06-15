bfcf351c71df19686356c438d2dea9e6b9d2081f
kevanoovitch <kiwikev00@gmail.com>
2025-06-15 21:58:55 +0200

    Basic Functionality

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Avalonia TODO-app tutorial
+This is just an app created by following the following sample tutorial:
+[Sample](https://github.com/AvaloniaUI/Avalonia.Samples/tree/main/src/Avalonia.Samples/CompleteApps/SimpleToDoList).
+
+# Dependencies 
+- .NET 9.0 SDK or higher
+
+# To run it 
+```
+$ cd TodoApp/
+$ dotnet run 
+```
--- TodoApp/ViewModels/MainWindowViewModel.cs
+++ TodoApp/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace TodoApp.ViewModels;
 
@@ -8,4 +10,27 @@ public partial class MainWindowViewModel : ViewModelBase
 
     public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } =
         new ObservableCollection<ToDoItemViewModel>();
+
+    // gets or set the content for new Items to add. If this is string not empty, the AddItemCmd will be enabled automatically
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(AddItemCommand))]
+    private string? _newItemContent;
+
+    //Returns if a new ITem can be added
+    private bool CanAddItem() => !string.IsNullOrWhiteSpace(NewItemContent);
+
+    [RelayCommand(CanExecute = nameof(CanAddItem))]
+    private void AddItem()
+    {
+        ToDoItems.Add(new ToDoItemViewModel() { Content = NewItemContent });
+
+        NewItemContent = null;
+    }
+
+    [RelayCommand]
+    private void RemoveItem(ToDoItemViewModel item)
+    {
+        // Remove the given item from the list
+        ToDoItems.Remove(item);
+    }
 }
